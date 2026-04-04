9ca8d551d4cf1b1207d04d4e1ee5921de02beb3e
Admayah <mayobami49@gmail.com>
2026-04-05 00:45:26 +0100

    added environment aware error handling

--- backend/src/middlewares/error.middleware.ts
+++ backend/src/middlewares/error.middleware.ts
@@ -1,18 +1,22 @@
 import type { NextFunction, Request, Response } from "express";
 import { AppError } from "../shared/errors.js";
 
+const isDev = process.env.NODE_ENV !== "production";
+
 export const errorHadnler = (err: any, req: Request, res: Response, next: NextFunction) => {
     console.error(err);
 
     if(err instanceof AppError) {
         return res.status(err.statusCode).json({
             success: false,
-            message: err.message
+            message: err.message,
+            ...(isDev && { stack: err.stack })
         });
     }
 
     return res.status(500).json({
         success: false,
-        message: "Internal Server Error"
+        message: "Internal Server Error",
+        ...(isDev && { stack: err.stack })
     });
 }
